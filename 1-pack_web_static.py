2ad4befe9661b68b2e4326992f41a1dda1e20130
tony <tony@computer.com>
2023-01-13 16:00:39 +0100

    solution to task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Generates a .tgz archive file from web_static folder
+"""
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """
+        Generates archive files and stores them in version folder
+    """
+    try:
+        time = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+        arc_name = "versions/web_static_{}.tgz".format(time)
+
+        local("mkdir -p versions")
+        arc_result = local("tar -cvzf {} web_static/".format(arc_name), capture=True)
+        
+        return arc_result
+    except Exception:
+        return None
