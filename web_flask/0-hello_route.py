2a1ae8ff0f1fea99914a423e8865556070a19f18
tony <tony@computer.com>
2023-01-15 20:03:46 +0100

    solution to task 0 web_flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+"""
+Contains a route that returns a string
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """
+        / route that returns "Hello HBNB!"
+    """
+    return "Hello HBNB!"
