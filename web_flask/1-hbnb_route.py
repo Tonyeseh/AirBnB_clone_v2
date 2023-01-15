08abfa8f4550aba02669da37b39e638cb5859a46
tony <tony@computer.com>
2023-01-15 20:35:02 +0100

    corrected task 1 web_static

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,21 @@
+#!/usr/bin/python3
 """ has routes / and /hbnb"""
 
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
     """ returns "Hello HBNB" """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_HBNB():
     """ returns "HBNB" """
     return "HBNB"
+
+
+if __name__ = "__main__":
+    app.run()
