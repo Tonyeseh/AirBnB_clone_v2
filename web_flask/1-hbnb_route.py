46ba83b23229911cf07a00cd0ffc3f458082fe62
tony <tony@computer.com>
2023-01-15 20:08:15 +0100

    solution to task 1 web_flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,14 @@
+""" has routes / and /hbnb"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """ returns "Hello HBNB" """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def display_HBNB():
+    """ returns "HBNB" """
+    return "HBNB"
