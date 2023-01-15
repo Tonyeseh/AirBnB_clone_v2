53ccd647bcb11cf2b6a11116b4c1a1d938bf506f
tony <tony@computer.com>
2023-01-15 22:25:58 +0100

    solution to task 7 web_flask

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -76,3 +76,7 @@ class FileStorage:
             del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
             pass
+
+    def close(self):
+        """calls the reload() method for deserializing the JSON file to objects"""
+        self.reload()
