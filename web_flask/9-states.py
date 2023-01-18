fd0717016309769c1e652b15909c13a2e120e64f
tony <tony@github.com>
2023-01-18 16:13:26 +0000

    solution to task 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+defines the route /states
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown(self):
+	"""
+	closes db session
+	"""
+	storage.close()
+
+@app.route('/states/', strict_slashes=False)
+def states():
+	"""route to all states"""
+	states = storage.all('State')
+	return render_template('9-states.html', states=states)
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id):
+	"""displays a state with it's cities"""
+	states = storage.all('State')
+	unique_state = None
+	for state in states.values():
+		if state.id == id:
+			unique_state = state
+			break
+	if unique_state:
+		return render_template('9-states.html', state=unique_state)
+	else:
+		return render_template('9-states.html', error='Not Found!')
+
+if __name__ == '__main__':
+	app.run()
