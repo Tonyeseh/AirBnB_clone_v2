ea6cc6b5cc5fa4e8c0be296122caa51fad1ef341
tony <tony@github.com>
2023-01-18 14:56:36 +0000

    solution to task 7
    defined route and the html page to return all states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+defines the route /state_list
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown(self):
+	"""
+	closes and starts a sql session
+	"""
+	storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+	"""
+	returns a html page of all states for routes /states_list
+	"""
+	states = storage.all('State')
+	return render_template('7-states_list.html', states=states)
+
+if __name__ == '__main__':
+	app.run()
