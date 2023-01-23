f3dcece63d4a2d9cf9462617a21e12d8a2332f1f
tony <tony@computer.com>
2023-01-23 21:50:32 +0100

    fixed style errors in 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,12 +7,14 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown(self):
     """
     closes and starts a sql session
     """
     storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
@@ -21,6 +23,7 @@ def states_list():
     """
     states = sorted(list(storage.all('State').values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
+
 
 if __name__ == '__main__':
     app.run()
