de9e1d0d264fd1b759de0bef2063dab5ee0fa554
tony <tony@computer.com>
2023-01-23 22:06:45 +0100

    fixed style errors

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,20 +7,23 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown(self):
-	"""
-	closes and starts a sql session
-	"""
-	storage.close()
+    """
+    closes and starts a sql session
+    """
+    storage.close()
+
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-	"""
-	returns a html page of all states for routes /states_list
-	"""
-	states = storage.all('State').values()
-	return render_template('8-cities_by_states.html', states=states)
+    """
+    returns a html page of all states for routes /states_list
+    """
+    states = storage.all('State').values()
+    return render_template('8-cities_by_states.html', states=states)
+
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
