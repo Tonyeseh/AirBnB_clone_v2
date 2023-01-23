5cd3913c75a4ae0d8ee080a8550f0f078e5cc21a
tony <tony@github.com>
2023-01-23 20:10:32 +0000

    removed excess tabs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,18 +9,18 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown(self):
-	"""
-	closes and starts a sql session
-	"""
-	storage.close()
+    """
+    closes and starts a sql session
+    """
+    storage.close()
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-	"""
-	returns a html page of all states for routes /states_list
-	"""
-	states = storage.all('State')
-	return render_template('7-states_list.html', states=states)
+    """
+    returns a html page of all states for routes /states_list
+    """
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
